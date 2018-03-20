bef0a763b66ed1085ba5e37144484e65c2fe6944
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2018-03-20 23:31:50 +0800

    Rename changelog to changelog.sh

--- changelog.sh
+++ changelog.sh
@@ -3,6 +3,7 @@
 source changelog-init.sh
 source changelog-new.sh
 source changelog-render.sh
+source changelog-release.sh
 
 if [ $1 == 'init' ]; then
   init ${@:1}
@@ -12,8 +13,10 @@ if [ $1 == 'new' ]; then
   new ${@:1}
 fi
 
-echo "asdf"
-
 if [ $1 == 'render' ]; then
   render ${@:1}
+fi
+
+if [ $1 == 'release' ]; then
+  release ${@:1}
 fi
