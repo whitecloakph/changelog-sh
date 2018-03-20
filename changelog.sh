c717571f8ce696182b742fc03cbc100c368924ee
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2018-03-20 23:35:52 +0800

    Add catch for no command

--- changelog.sh
+++ changelog.sh
@@ -5,18 +5,35 @@ source changelog-new.sh
 source changelog-render.sh
 source changelog-release.sh
 
-if [ $1 == 'init' ]; then
-  init ${@:1}
-fi
+function changelog {
+  error="
+  Invalid command. Valid commands:\n
+  - init\n
+  - new\n
+  - render\n
+  - release
+  "
 
-if [ $1 == 'new' ]; then
-  new ${@:1}
-fi
+  if [ "$#" -eq 0 ]; then
+    echo $error
+    return
+  fi
 
-if [ $1 == 'render' ]; then
-  render ${@:1}
-fi
+  if [ $1 == 'init' ]; then
+    init ${@:1}
+  fi
 
-if [ $1 == 'release' ]; then
-  release ${@:1}
-fi
+  if [ $1 == 'new' ]; then
+    new ${@:1}
+  fi
+
+  if [ $1 == 'render' ]; then
+    render ${@:1}
+  fi
+
+  if [ $1 == 'release' ]; then
+    release ${@:1}
+  fi
+}
+
+changelog $@
