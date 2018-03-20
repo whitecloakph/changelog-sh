edbb609dafb53e37717e5b123801ec30f763a868
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2018-03-20 23:52:10 +0800

    Add install.sh

---
+++ tools/install.sh
@@ -0,0 +1,15 @@
+if [ ! -n "$CHANGE" ]; then
+  CHANGE=~/.change
+fi
+
+if [ -d "$CHANGE" ]; then
+  printf "You already have CHANGE installed.\n"
+  printf "You'll need to remove $CHANGE if you want to re-install.\n"
+  exit
+fi
+
+git clone https://github.com/whitecloakph/changelog-sh.git $CHANGE
+
+'Entering sudo'
+sudo cp $CHANGE/changelog.sh /usr/local/bin/change
+sudo chmod +x /usr/local/bin/change 
